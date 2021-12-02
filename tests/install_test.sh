22364094d3bcf5864c9ec2c78bd78ae5adf19e6e
Ross Eastman <ross.eastman@catalyst-eu.net>
2021-12-02 14:43:12 +0000

    Spins up a container running 2104 and tests and runs WAYD

--- tests/install_test.sh
+++ tests/install_test.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-../install.sh
+cd /wayd/ && sudo ./install.sh
 
-if [ $@ -ne 0 ]; then
+if [ $? -ne 0 ]; then
     echo "ERROR: Cannot install WAYD"
+fi
+
+whatareyoudoing
+
+if [ $? -ne 0 ]; then
+    echo "ERROR: Cannot run WAYD"
 fi
